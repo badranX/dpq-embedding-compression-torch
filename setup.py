b25a73dc3c844309e6a92c7b92c3bc7da7850a8b
Yahya Badran <techtweaking@gmail.com>
2023-11-29 17:42:41 +0100

    add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(name="differentiable-product-quantization",
+      author = "Yahya Badran",
+      author_email = "yahya.badran@gmail.com",
+      version="0.1",
+      packages=find_packages(),
+      )
